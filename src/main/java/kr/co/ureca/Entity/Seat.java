af9b2c92d908203c4f3cd342ec89fb96c2eac6f4
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-08-28 15:24:40 +0900

    feat: 엔티티 필드 추가

--- src/main/java/kr/co/ureca/Entity/Seat.java
+++ src/main/java/kr/co/ureca/Entity/Seat.java
@@ -1,4 +1,31 @@
 package kr.co.ureca.Entity;
 
+import jakarta.persistence.*;
+import jdk.jfr.Description;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seat_id")
+    private Long seatId;
+
+    @Description("예약 상태")
+    @Builder.Default
+    private Boolean status = false;
+
+    @OneToOne
+    private User user;
+
+    private Long SeatNo;
+
 }
--- src/main/java/kr/co/ureca/Entity/User.java
+++ src/main/java/kr/co/ureca/Entity/User.java
@@ -1,4 +1,19 @@
 package kr.co.ureca.Entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @OneToOne(mappedBy = "user")
+    @JoinColumn(name = "seat_id")
+    private Seat seat;
+
+    private String userName;
+    private String email;
 }
