c5c0d66aa4a751ccc56d6624e6cbacfc10f23355
rlawotmd <kimjs2400@naver.com>
2024-08-29 13:08:12 +0900

    edit name

--- src/main/java/kr/co/ureca/repository/SeatRepository.java
+++ src/main/java/kr/co/ureca/repository/SeatRepository.java
@@ -1,9 +1,9 @@
 package kr.co.ureca.repository;
 
-import kr.co.ureca.entity.User;
+import kr.co.ureca.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface userRepository extends JpaRepository<User, Long> {
+public interface SeatRepository extends JpaRepository<Seat, Long> {
 }
--- src/main/java/kr/co/ureca/repository/UserRepository.java
+++ src/main/java/kr/co/ureca/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package kr.co.ureca.repository;
 
-import kr.co.ureca.entity.Seat;
+import kr.co.ureca.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface seatRepository extends JpaRepository<Seat, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
