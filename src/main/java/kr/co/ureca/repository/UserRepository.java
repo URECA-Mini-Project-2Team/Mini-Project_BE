2b4fc7a39e0ef6f92769dba22858610e844e6cd5
rlawotmd <kimjs2400@naver.com>
2024-09-09 21:18:52 +0900

    remove unused

--- src/main/java/kr/co/ureca/repository/UserRepository.java
+++ src/main/java/kr/co/ureca/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package kr.co.ureca.repository;
 
-import kr.co.ureca.entity.Seat;
 import kr.co.ureca.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
