d85740eaca538d8b3fcb30ef3b58c7538016f80c
rlawotmd <kimjs2400@naver.com>
2024-09-12 17:00:00 +0900

    add Exception Handling

--- src/main/java/kr/co/ureca/service/UserService.java
+++ src/main/java/kr/co/ureca/service/UserService.java
@@ -28,11 +28,12 @@ public class UserService {
         User user;
         if (userOptional.isPresent()) { // User가 이미 있음
             user = userOptional.get();
+            if (!user.getName().equals(reservationDto.getUserName())) throw  new Exception("이미 사용중인 아이디입니다.");
             if (!user.getPassword().equals(reservationDto.getPassword())) throw new Exception("비밀번호가 틀렸습니다.");
         } else { // User가 없어서 새로 생성
             user = User.builder()
                     .nickName(reservationDto.getNickName())
-                    .userName(reservationDto.getUserName())
+                    .name(reservationDto.getNickName())
                     .password(reservationDto.getPassword())
                     .build();
             register(user);
@@ -51,7 +52,7 @@ public class UserService {
     }
 
     public void updateUserStatus(User user) {
-        user.updateUserStatus(user);
+        user.updateUserStatus();
         userRepository.save(user);
     }
 }
