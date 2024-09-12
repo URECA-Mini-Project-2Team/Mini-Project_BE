5c237e378bec1d2f1cdf1c9b5b5ca476de32597b
rlawotmd <kimjs2400@naver.com>
2024-09-12 15:44:21 +0900

    add builder

--- src/main/java/kr/co/ureca/entity/Seat.java
+++ src/main/java/kr/co/ureca/entity/Seat.java
@@ -1,14 +1,14 @@
 package kr.co.ureca.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "seatdb")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Seat {
 
     @Id
